76ee2484820e078dcacb89b1e39431d04a894fad
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-03-22 14:49:05 -0400

    feat: add express setup

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,51 @@
-import { Client, GatewayIntentBits, Events } from 'discord.js';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { Client, Events, GatewayIntentBits } from 'discord.js';
 import * as dotenv from 'dotenv';
+import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 dotenv.config();
+
+// -------------------------------------------------------------------------
+// Express server
+// -------------------------------------------------------------------------
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(
+  helmet({
+    crossOriginEmbedderPolicy: true,
+    contentSecurityPolicy: {
+      directives: helmet.contentSecurityPolicy.getDefaultDirectives(),
+    },
+  }),
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '5mb' }));
+app.use(morgan('dev'));
+app.use(cors());
+
+app.get('/', (_req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  const url = `http://localhost:${process.env.PORT}`;
+  console.log(`Server running at ${url} 🚀`);
+});
+
+// -------------------------------------------------------------------------
+// Discord client
+// -------------------------------------------------------------------------
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, (readyClient) => {
-  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+  console.log(`Logged in as ${readyClient.user.tag} 🤖`);
 });
 
 client.login(process.env.BOT_TOKEN);
